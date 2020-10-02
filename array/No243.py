40d78ccec6259d72b4bfb0ec859e51f14d8c02ea
BJTU17221294 <839891341@qq.com>
2020-10-02 22:40:04 +0800

    No243 easy

---
+++ array/No243.py
@@ -0,0 +1,30 @@
+#
+# Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+#
+# Example:
+# Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+#
+# Input: word1 = “coding”, word2 = “practice”
+# Output: 3
+# Input: word1 = "makes", word2 = "coding"
+# Output: 1
+
+class Solution(object):
+    def shortestDistance(self, words, word1, word2):
+        """
+        :type words: List[str]
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        w1, w2 = -1, -1
+        minDis = float('inf')
+        for i in range(len(words)):
+            if word1 == words[i]:
+                w1 = i
+            elif word2 == words[i]:
+                w2 = i
+            if w1 != -1 and w2 != -1:
+                minDis = min(abs(w2 - w1), minDis)
+        return minDis
+
