a61b3c1f018eab55e83d705e278b68df84d03844
BJTU17221294 <839891341@qq.com>
2020-10-31 20:51:39 +0800

    Logger_Rate_limit_system.py

---
+++ Hashmap/Logger_Rate_limit_system.py
@@ -0,0 +1,26 @@
+# very easy question to solve
+# Here is the answer from leetcode
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._msg_dict = {}
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        """
+        if message not in self._msg_dict:
+            # case 1). add the message to print
+            self._msg_dict[message] = timestamp
+            return True
+
+        if timestamp - self._msg_dict[message] >= 10:
+            # case 2). update the timestamp of the message
+            self._msg_dict[message] = timestamp
+            return True
+        else:
+            return False
