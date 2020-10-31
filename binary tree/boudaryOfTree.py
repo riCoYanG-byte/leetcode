746403674b6c8092f50074f09de9da6e705349b2
BJTU17221294 <839891341@qq.com>
2020-10-31 21:30:34 +0800

    boundyOfTree

---
+++ binary tree/boudaryOfTree.py
@@ -0,0 +1,62 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isLeaves(self,node):
+        if node.left is None and node.right is None:
+            return True
+        else:
+            return False
+
+    #add leaves
+    # by using dfs we can make sure the leaves
+    def addLeaves(self,node,res):
+        if self.isLeaves(node):
+            res.append(node)
+        else:
+            if node.left:
+                self.addLeaves(node,res)
+            else:
+                self.addLeaves(node,res)
+
+    # divide it into three parts
+    def boundaryOfBinaryTree(self, root):
+        # dive into the  leftmost
+        node = root
+        res =  [node]
+
+        node = root.left
+        while node:
+            if self.isLeaves(node):
+                res.append(node)
+            if node.left:
+                node = node.left
+            else:
+                node = node.right
+
+        # add leaves
+        self.addLeaves(root,res)
+
+        #get right most by using stack
+        stack = []
+        node = root.right
+        while node:
+            if self.isLeaves(node):
+                stack.append(node)
+            if node.right:
+                node = node.right
+            else:
+                node = node.left
+
+        #add stack
+        while stack:
+            res.append(stack.pop())
+
+        return res
+
+
+
