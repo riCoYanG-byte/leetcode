295c48ea3ba6de8dd146eb55fe283b7b23a99c25
BJTU17221294 <839891341@qq.com>
2020-11-07 15:20:58 +0800

    No.159.py

--- array/SlidingWindow/No.159.py
+++ array/SlidingWindow/No.159.py
@@ -3,3 +3,29 @@
 # Input: "eceba"
 # Output: 3
 # Explanation: t is "ece" which its length is 3.
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def lengthOfLongestSubstringTwoDistinct(self, s):
+
+        if len(s) < 3:
+            return len(s)
+
+        left, right, max_len = 0, 0, 2
+        hashmap = defaultdict()
+
+        while right < len(s):
+            if len(hashmap) < 3:
+                hashmap[s[right]] = right
+                right += 1
+            if len(hashmap) == 3:
+                min_value = min(hashmap.values())
+                del hashmap[s[min_value]]
+                left = min_value + 1
+
+            max_len = max(max_len, right - left)
+
+        return max_len
+
