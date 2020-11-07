773863b05a9b2120fa30fa5efe14e64119fe0ae5
BJTU17221294 <839891341@qq.com>
2020-11-07 15:39:45 +0800

    No.978.py

---
+++ array/SlidingWindow/No.978.py
@@ -0,0 +1,29 @@
+# A subarray A[i], A[i+1], ..., A[j] of A is said to be turbulent if and only if:
+#
+# For i <= k < j, A[k] > A[k+1] when k is odd, and A[k] < A[k+1] when k is even;
+# OR, for i <= k < j, A[k] > A[k+1] when k is even, and A[k] < A[k+1] when k is odd.
+# That is, the subarray is turbulent if the comparison sign flips between each adjacent pair of elements in the subarray.
+#
+# Return the length of a maximum size turbulent subarray of A.
+#
+# 灵活运用它cmp函数 :
+# cmp(x,y) 函数用于比较2个对象，如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1。
+from filecmp import cmp
+
+from cffi.backend_ctypes import xrange
+
+
+class Solution(object):
+    def maxTurbulenceSize(self, A):
+        N = len(A)
+        ans = 1
+        anchor = 0
+
+        for i in xrange(1, N):
+            c = cmp(A[i-1], A[i])
+            if c == 0:
+                anchor = i
+            elif i == N-1 or c * cmp(A[i], A[i+1]) != -1:
+                ans = max(ans, i - anchor + 1)
+                anchor = i
+        return ans
