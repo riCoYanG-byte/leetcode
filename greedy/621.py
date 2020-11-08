b721b64bbd1fffdaf38413c0876e06ed804f5e5f
BJTU17221294 <839891341@qq.com>
2020-11-08 16:25:47 +0800

    really difficult

---
+++ greedy/621.py
@@ -0,0 +1,26 @@
+# Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+#
+# However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+#
+# Return the least number of units of times that the CPU will take to finish all the given tasks.
+from typing import List
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # frequencies of the tasks
+        frequencies = [0] * 26
+        for t in tasks:
+            frequencies[ord(t) - ord('A')] += 1
+
+        frequencies.sort()
+
+        # max frequency
+        f_max = frequencies.pop()
+        idle_time = (f_max - 1) * n
+
+        while frequencies and idle_time > 0:
+            idle_time -= min(f_max - 1, frequencies.pop())
+        idle_time = max(0, idle_time)
+
+        return idle_time + len(tasks)
