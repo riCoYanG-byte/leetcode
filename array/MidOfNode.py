cf32654d7b5f3963fac4bd4d6c542dcc104aa763
BJTU17221294 <839891341@qq.com>
2020-12-11 15:43:05 +0800

    middle of Node

---
+++ array/MidOfNode.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
